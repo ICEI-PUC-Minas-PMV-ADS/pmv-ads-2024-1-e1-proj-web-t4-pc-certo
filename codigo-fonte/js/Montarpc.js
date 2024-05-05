db0787f6439cc96b57e2b35fcf9a8955ad1970dc
LuanBz <lbezerra@sga.pucminas.br>
2024-05-05 02:05:00 -0300

    montar pc

--- codigo-fonte/js/Montarpc.js
+++ codigo-fonte/js/Montarpc.js
@@ -1,3 +1,15 @@
-function Selecionar() {
-    document.getElementsByClassName("card").style.color= "card:active"
-}
+// const cardEL = document.querySelector('.card');
+//
+// cardEL.addEventListener('click', () => {
+//    cardEL.classList.add('active');
+// });
+
+const cardElList = document.querySelectorAll('.card');
+
+cardElList.forEach(cardEL => {
+    cardEL.addEventListener('click', () => {
+        document.querySelector('.active')?.classList.remove('active');
+        cardEL.classList.add('active');
+    });
+})
+
