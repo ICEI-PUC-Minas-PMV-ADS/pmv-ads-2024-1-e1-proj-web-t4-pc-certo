16cbabcc81e12ea1ad7d6e92837d0e06ca3ebb13
LcsToti <Lucasgabrielft01@gmail.com>
2024-05-08 20:36:00 -0300

    Revert "a"
    This reverts commit cb02d4d148f7c7aac8d511a4c70f2c2c342a9004.

--- codigo-fonte/js/Montarpc.js
+++ codigo-fonte/js/Montarpc.js
@@ -1,1 +1,15 @@
+// const cardEL = document.querySelector('.card');
+//
+// cardEL.addEventListener('click', () => {
+//    cardEL.classList.add('active');
+// });
+
+const cardElList = document.querySelectorAll('.card');
+
+cardElList.forEach(cardEL => {
+    cardEL.addEventListener('click', () => {
+        document.querySelector('.active')?.classList.remove('active');
+        cardEL.classList.add('active');
+    });
+})
 
