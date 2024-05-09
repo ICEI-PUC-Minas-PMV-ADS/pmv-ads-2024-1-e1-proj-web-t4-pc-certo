e12df07adbd3d866c97e85b3bb1aad3f4c17d04b
LuanBz <lbezerra@sga.pucminas.br>
2024-05-09 00:53:49 -0300

    montar pc atualização
    agora é possível selecionar o botão e automaticamente rola a página para baixo.

--- codigo-fonte/js/Montarpc.js
+++ codigo-fonte/js/Montarpc.js
@@ -1,15 +1,11 @@
-// const cardEL = document.querySelector('.card');
-//
-// cardEL.addEventListener('click', () => {
-//    cardEL.classList.add('active');
-// });
-
 const cardElList = document.querySelectorAll('.card');
 
 cardElList.forEach(cardEL => {
     cardEL.addEventListener('click', () => {
+        window.scroll({top: innerHeight, behavior: "smooth"});
         document.querySelector('.active')?.classList.remove('active');
         cardEL.classList.add('active');
+        document.getElementById('orcamento').style.display = "flex";
     });
 })
 
