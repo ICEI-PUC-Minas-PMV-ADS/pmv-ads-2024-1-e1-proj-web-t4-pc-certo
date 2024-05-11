c1740aa39b20c7de3408e9aefd30bc9515c27f54
Pedro Henrique Machado <pedroh.machado800@gmail.com>
2024-05-11 03:15:39 -0300

    Cadastro funcionando

--- codigo-fonte/js/Cadastro.js
+++ codigo-fonte/js/Cadastro.js
@@ -1,9 +1,94 @@
-const form = document.querySelector('.form');
-const obrigatorio = document.querySelectorAll('.obrigatorio');
-const mensagemErro = document.querySelectorAll('.erro');
-const email = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
-const checkbox = document.querySelector('.checkbox');
+// VARIÁVEIS
+var form = document.querySelector('.form');
+var obrigatorio = document.querySelectorAll('.obrigatorio');
+var mensagemErro = document.querySelectorAll('.erro');
+var email = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+var checkbox = document.querySelector('.checkbox');
+var cadastrar = document.querySelector('.Cadastrar');
 
+
+// FUNÇÕES PARA MOSTRAR O ERRO
+function erro(index) {
+    mensagemErro[index].style.opacity = '1.0';
+    obrigatorio[index].style.border = '2px solid #e63636';
+}
+
+function termosErro() {
+    mensagemErro[4].style.opacity = '1.0';
+}
+
+// FUNÇÕES PARA TIRAR O ERRO
+function removerErro(index) {
+    mensagemErro[index].style.opacity = '0';
+    obrigatorio[index].style.border = '';
+}
+
+function removerTermosErro() {
+    mensagemErro[4].style.opacity = '0';
+}
+
+// FUNÇÕES PARA VALIDAR OS CAMPOS
+function validarNome() {
+    if (obrigatorio[0].value.length < 3) {
+        erro(0);
+        localStorage.removeItem("nomeCadastro");
+    }
+    else {
+        removerErro(0);
+        var nome = obrigatorio[0].value;
+        localStorage.setItem("nomeCadastro", nome);
+    }
+}
+
+function validarEmail() {
+    if (!email.test(obrigatorio[1].value)) {
+        erro(1);
+        localStorage.removeItem("emailCadastro");
+    }
+    else {
+        removerErro(1);
+        let email = obrigatorio[1].value;
+        localStorage.setItem("emailCadastro", email);
+    }
+}
+
+function validarSenha() {
+    if (obrigatorio[2].value.length < 8) {
+        erro(2);
+        localStorage.removeItem("senhaCadastro");
+    }
+    else {
+        removerErro(2);
+        var senha = obrigatorio[2].value;
+        localStorage.setItem("senhaCadastro", senha);
+    }
+}
+
+function validarConfirmacao() {
+    if (obrigatorio[3].value !== obrigatorio[2].value || obrigatorio[3].value.length < 8) {
+        erro(3);
+        localStorage.removeItem("confirmacaoCadastro");
+    }
+    else {
+        removerErro(3);
+        var confirmacao = obrigatorio[3].value;
+        localStorage.setItem("confirmacaoCadastro", confirmacao);
+    }
+}
+
+function validarTermos() {
+    if (!checkbox.checked) {
+        termosErro();
+        localStorage.removeItem("termosCadastro");
+    }
+    else {
+        removerTermosErro();
+        localStorage.setItem("termosCadastro", "Aceitou os termos de uso");
+    }
+}
+
+
+// VALIDAÇÃO DO CADASTRO
 form.addEventListener('submit', (event) => {
     event.preventDefault();
     validarNome();
@@ -11,58 +96,12 @@ form.addEventListener('submit', (event) => {
     validarSenha();
     validarConfirmacao();
     validarTermos();
+    var validado0 = localStorage.getItem("nomeCadastro");
+    var validado1 = localStorage.getItem("emailCadastro");
+    var validado2 = localStorage.getItem("senhaCadastro");
+    var validado3 = localStorage.getItem("confirmacaoCadastro");
+    var validado4 = localStorage.getItem("termosCadastro");
+    if (validado0 && validado1 && validado2 && validado3 && validado4) {
+        window.location.href = "../codigo-fonte/Login.html"
+    }
 })
-
-function erro(index) {
-    mensagemErro[index].style.opacity = '1.0';
-    obrigatorio[index].style.border = '2px solid #e63636';
-}
-
-function removerErro(index) {
-    mensagemErro[index].style.opacity = '0';
-    obrigatorio[index].style.border = '';
-}
-
-function validarNome() {
-    if(obrigatorio[0].value.length < 3){
-        erro(0);
-    }
-    else {
-        removerErro(0);
-    }
-}
-
-function validarEmail() {
-    if(!email.test(obrigatorio[1].value))
-        erro(1);
-    else {
-        removerErro(1);
-    }
-}
-
-function validarSenha() {
-    if(obrigatorio[2].value.length < 8) {
-        erro(2);
-    }
-    else {
-        removerErro(2);
-    }
-}
-
-function validarConfirmacao() {
-    if(obrigatorio[3].value !== obrigatorio[2].value || obrigatorio[3].value.length < 8) {
-        erro(3);
-    }
-    else {
-        removerErro(3);
-    }
-}
-
-function validarTermos() {
-    if(checkbox.checked) {
-        console.log('FOI');
-    }
-    else (
-        console.log('marca aí paizao')
-    )
-}
