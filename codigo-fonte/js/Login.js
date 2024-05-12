916fb76aaa332fa425196992f7e9c8c5f026e5c9
Caeetanoo <caetano20062@gmail.com>
2024-05-11 21:44:17 -0300

    SALVANDO PRA NAO DAR MERDA( login 95%)

--- codigo-fonte/js/Login.js
+++ codigo-fonte/js/Login.js
@@ -2,3 +2,38 @@ console.log(localStorage.getItem("nomeCadastro"));
 console.log(localStorage.getItem("emailCadastro"));
 console.log(localStorage.getItem("senhaCadastro"));
 console.log(localStorage.getItem("confirmacaoCadastro"));
+// variaveis
+var erroremail = document.getElementById("errorEmail");
+var redborder = document.querySelectorAll('#email, #senha');
+var inputs = document.querySelectorAll('input')
+var emailInserido = document.getElementById("email");
+var senhaInserida = document.getElementById('senha')
+var botaologin = document.getElementById('botao');
+
+
+
+// validar emails cadastrados
+botaologin.addEventListener('click', function validarEmail() {
+    if (localStorage.getItem("emailCadastro") == emailInserido && (localStorage.getItem("senhaCadastro")) == senhaInserida) { console.log('o email é valido') }
+
+    else {
+        console.log('email invalido');
+        erroremail.style.display = "block";
+        redborder.forEach(function (element) {
+            element.style.border = "2px solid #e63636";
+        });
+    }
+
+}
+)
+//remover erro
+inputs.forEach(function(input){
+    input.addEventListener('click', function(){
+        erroremail.style.display = "none";
+        redborder.forEach(function(element){
+            element.style.border = "0px";
+        })
+
+    })
+})
+
