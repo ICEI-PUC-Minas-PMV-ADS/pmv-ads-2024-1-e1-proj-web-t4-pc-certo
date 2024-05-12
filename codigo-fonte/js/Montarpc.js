c11c69a235e82fa2d5cacac1c94540b9c265e460
LuanBz <lbezerra@sga.pucminas.br>
2024-05-11 21:07:37 -0300

    responsividade dos cards 100% sem bugs

--- codigo-fonte/js/Montarpc.js
+++ codigo-fonte/js/Montarpc.js
@@ -6,6 +6,7 @@ btncardElList.forEach(btnCardEl => {
     btnCardEl.addEventListener('click', () => {
         window.scroll({top: innerHeight, behavior: "smooth"});
         document.querySelector('.active')?.classList.remove('active');
+        document.querySelector('.ativado')?.classList.remove('ativado');
         btnCardEl.classList.add('active');
     });
 })
@@ -18,6 +19,9 @@ btncardElList[0].addEventListener('click', () => {
     document.getElementById('PriceMin').style.display = "none";
     document.getElementById('PriceMed').style.display = "none";
     document.getElementById('PriceMax').style.display = "none";
+    document.getElementById('avancar').style.display = "none";
+    document.getElementById('avancar1').style.display = "none";
+    document.getElementById('avancar2').style.display = "none";
 })
 
 btncardElList[1].addEventListener('click', () => {
@@ -28,6 +32,9 @@ btncardElList[1].addEventListener('click', () => {
     document.getElementById('PriceMin').style.display = "none";
     document.getElementById('PriceMed').style.display = "none";
     document.getElementById('PriceMax').style.display = "none";
+    document.getElementById('avancar').style.display = "none";
+    document.getElementById('avancar1').style.display = "none";
+    document.getElementById('avancar2').style.display = "none";
 })
 
 btncardElList[2].addEventListener('click', () => {
@@ -38,6 +45,9 @@ btncardElList[2].addEventListener('click', () => {
     document.getElementById('PriceMin').style.display = "none";
     document.getElementById('PriceMed').style.display = "none";
     document.getElementById('PriceMax').style.display = "none";
+    document.getElementById('avancar').style.display = "none";
+    document.getElementById('avancar1').style.display = "none"; 
+    document.getElementById('avancar2').style.display = "none";
 })
 
 // Escolha das predefinições de orçamento
@@ -45,6 +55,7 @@ const btnOrcamentoElList = document.querySelectorAll('.btnOrcamento');
 
 btnOrcamentoElList.forEach(btnOrcamentoEl => {
     btnOrcamentoEl.addEventListener('click', () => {
+        window.scrollTo(0, 1000)
         document.querySelector('.ativo')?.classList.remove('ativo');
         btnOrcamentoEl.classList.add('ativo');
     });
@@ -54,54 +65,63 @@ btnOrcamentoElList[0].addEventListener('click', () => {
     document.getElementById('PriceMin').style.display = "flex";
     document.getElementById('PriceMed').style.display = "none";
     document.getElementById('PriceMax').style.display = "none";
+    document.getElementById('avancar').style.display = "none";
 })
 
 btnOrcamentoElList[1].addEventListener('click', () => {
     document.getElementById('PriceMin').style.display = "none";
     document.getElementById('PriceMed').style.display = "flex";
     document.getElementById('PriceMax').style.display = "none";
+    document.getElementById('avancar').style.display = "none";
 })
 
 btnOrcamentoElList[2].addEventListener('click', () => {
     document.getElementById('PriceMin').style.display = "none";
     document.getElementById('PriceMed').style.display = "none";
     document.getElementById('PriceMax').style.display = "flex";
+    document.getElementById('avancar').style.display = "none";
 })
 // caso a escolha seja profissional
 btnOrcamentoElList[3].addEventListener('click', () => {
     document.getElementById('PriceMin').style.display = "flex";
     document.getElementById('PriceMed').style.display = "none";
     document.getElementById('PriceMax').style.display = "none";
+    document.getElementById('avancar').style.display = "none";
 })
 
 btnOrcamentoElList[4].addEventListener('click', () => {
     document.getElementById('PriceMin').style.display = "none";
     document.getElementById('PriceMed').style.display = "flex";
     document.getElementById('PriceMax').style.display = "none";
+    document.getElementById('avancar').style.display = "none";
 })
 
 btnOrcamentoElList[5].addEventListener('click', () => {
     document.getElementById('PriceMin').style.display = "none";
     document.getElementById('PriceMed').style.display = "none";
     document.getElementById('PriceMax').style.display = "flex";
+    document.getElementById('avancar').style.display = "none";
 })
 // caso a escolha seja casual
 btnOrcamentoElList[6].addEventListener('click', () => {
     document.getElementById('PriceMin').style.display = "flex";
     document.getElementById('PriceMed').style.display = "none";
     document.getElementById('PriceMax').style.display = "none";
+    document.getElementById('avancar').style.display = "none";
 })
 
 btnOrcamentoElList[7].addEventListener('click', () => {
     document.getElementById('PriceMin').style.display = "none";
     document.getElementById('PriceMed').style.display = "flex";
     document.getElementById('PriceMax').style.display = "none";
+    document.getElementById('avancar').style.display = "none";
 })
 
 btnOrcamentoElList[8].addEventListener('click', () => {
     document.getElementById('PriceMin').style.display = "none";
     document.getElementById('PriceMed').style.display = "none";
     document.getElementById('PriceMax').style.display = "flex";
+    document.getElementById('avancar').style.display = "none";
 })
 
 // Botão de preço
@@ -110,6 +130,42 @@ const btnPriceElList = document.querySelectorAll('.btnPrice');
 btnPriceElList.forEach(btnPriceEl => {
     btnPriceEl.addEventListener('click', () => {
         document.querySelector('.ativado')?.classList.remove('ativado');
-        btnPriceEl.classList.add('ativado');
-    });
+        btnPriceEl.classList.add('ativado');    
+    })
+})
+
+btnPriceElList[0].addEventListener('click', () => {
+    document.getElementById('avancar').style.display = "flex";
+})
+
+btnPriceElList[1].addEventListener('click', () => {
+    document.getElementById('avancar').style.display = "flex";
+})
+
+btnPriceElList[2].addEventListener('click', () => {
+    document.getElementById('avancar').style.display = "flex";
+})
+
+btnPriceElList[3].addEventListener('click', () => {
+    document.getElementById('avancar1').style.display = "flex";
+})
+
+btnPriceElList[4].addEventListener('click', () => {
+    document.getElementById('avancar1').style.display = "flex";
+})
+
+btnPriceElList[5].addEventListener('click', () => {
+    document.getElementById('avancar1').style.display = "flex";
+})
+
+btnPriceElList[6].addEventListener('click', () => {
+    document.getElementById('avancar2').style.display = "flex";
+})
+
+btnPriceElList[7].addEventListener('click', () => {
+    document.getElementById('avancar2').style.display = "flex";
+})
+
+btnPriceElList[8].addEventListener('click', () => {
+    document.getElementById('avancar2').style.display = "flex";
 })
