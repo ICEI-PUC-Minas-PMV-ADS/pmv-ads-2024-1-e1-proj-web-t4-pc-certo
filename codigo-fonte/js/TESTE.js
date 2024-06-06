12f24a8a74ce7b8155663c4e04e332126376b0b1
PedroBzra <pedro.fernandobzra@gmail.com>
2024-06-06 11:59:53 -0300

    Grandes mudanças no funcionamento da página
    Em fase de teste

--- codigo-fonte/js/TESTE.js
+++ codigo-fonte/js/TESTE.js
@@ -1,34 +1,52 @@
+// RESETA O TOTALPARCIL AO RECOMEÇAR
+localStorage.setItem('totalParcial', 0)
+
+// VARIAVEIS
 let componentes = {};
 let componenteAtual = "Processador";
 let componenteSelecionado = {};
-let precoTotal = 0
+let precoParcial = parseFloat(localStorage.getItem('totalParcial')) || 0;
+let precoTemporario = 0;
 
+// ATUALIZA O TOTAL PARCIAL DA TELA
+function atualizarTotalParcial() {
+    const TotalParcial = document.getElementById('parcialTotal');
+    TotalParcial.innerHTML = `R$${precoParcial}`;
+}
+
+// SALVA COMPONENTES NO LOCAL STORAGE
+function salvarComponentesSelecionados() {
+    localStorage.setItem('componentesSelecionados', JSON.stringify(componenteSelecionado));
+}
+
+// CARREGAR JSON
 async function carregarDataComponentes() {
     const resposta = await fetch('/codigo-fonte/js/JSON/Componentes.json');
     componentes = await resposta.json();
     carregarComponentes(componenteAtual);
 }
-
+// PEGAR O TIPO DE COMPONENTE ESPECIFICO NO JSON
 function carregarComponentes(tipo) {
     const container = document.querySelector('#containerComp')
     container.innerHTML = ''
 
+    // CRIACAO DE HTML COM JSON
     componentes[tipo].forEach((componente) => {
-
-        // CRIACAO DE HTML COM JSON
         const opcao = document.createElement('div')
-
         opcao.classList.add('opcaoComponente')
+
+        // FORMATANDO VALOR PARA BRL
         const formatarValor = (preco) => {
             return componente.preco.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
         };
 
-        componente.preco = formatarValor(componente.preco);
+        const precoFormatado = formatarValor(componente.preco);
 
+        // CRIACAO DAS OPCOES COM JSON
         opcao.innerHTML = `
             <div class="nomePreco">
                 <h2 class="nome">${componente.modelo}</h2>
-                <h2 class="preco">R$${componente.preco}</h2>
+                <h2 class="preco">R$${precoFormatado}</h2>
             </div>
             <div class="imgComponente"><img class="imagemComponente" src="${componente.imagem}">
             </div>
@@ -43,18 +61,23 @@ function carregarComponentes(tipo) {
         `;
 
 
-        // DINAMICA NA DIV OPCAO AO SELECIONAR O +
+        // DINAMICA NA DIV OPCAO AO SER SELECIONADA
         opcao.querySelector('.addComponente').addEventListener('click', () => {
             document.querySelectorAll('.opcaoComponente').forEach(opc => { opc.classList.remove('opcaoSelecionada'); });
             opcao.classList.add('opcaoSelecionada');
 
             componenteSelecionado[tipo] = componente;
 
-            // Atualizar o preço total
-            attValorTotal(componente.preco);
+            // ALTERANDO O VALOR TOTAL PARCIAL AO CLICAR NA OPCAO
+            precoTemporario = parseFloat(componente.preco);
+            const TotalParcial = document.getElementById('parcialTotal');
+            TotalParcial.innerHTML = `R$${(precoParcial + precoTemporario)}`;
+
+            salvarComponentesSelecionados();
         });
         container.appendChild(opcao);
 
+        // CALC DESEMPENHO MUDANDO COR
         function lvlDesempenho() {
             const alerta = opcao.querySelector(".alertaDesempenho")
 
@@ -73,92 +96,77 @@ function carregarComponentes(tipo) {
         }
         lvlDesempenho()
 
+        // ORC TOTAL ESCOLHIDO NA PAG MONTAR PC
         function orcTotal() {
             let orcTot = localStorage.getItem('orcamentoTotal')
             document.getElementById('orcTotal').textContent = orcTot
         }
         orcTotal()
-
-
     })
 }
-
-// Atualizar o preço total
-function attValorTotal(preco) {
-    const numericPrice = parseFloat(preco.replace('R$', '').replace('.', '').replace(',', '.'));
-    precoTotal += numericPrice;
-
-    // Salvar o total no localStorage
-    localStorage.setItem('Total Parcial', precoTotal);
-
-    // Atualizar o campo "Total Parcial" na interface
-    document.getElementById('parcialTotal').innerText = `R$ ${precoTotal.toFixed(2)}`;
-}
-
-// Recuperar o valor total do localStorage ao carregar a página
-function carregarValorTotal() {
-    const ValorTotalSalvo = localStorage.getItem('precoTotal');
-    if (ValorTotalSalvo) {
-        precoTotal = parseFloat(ValorTotalSalvo);
-        document.getElementById('parcialTotal').innerText = `R$ ${precoTotal.toFixed(2)}`;
-    }
-}
-
+// MUDAR COMPONENTES AO CLICAR EM AVANCAR
 document.getElementById('avancar').addEventListener('click', () => {
-
     if (!componenteSelecionado[componenteAtual]) {
         alert("Por favor, selecione uma opção antes de avançar.");
         return;
     }
-    if (componenteAtual === 'Processador') {
-        componenteAtual = 'Placa-mae';
-        document.getElementById('resumoTit').innerText = 'Placa-Mãe';
-        document.getElementById('resumoDesc').innerText = 'É como o centro de controle do computador, onde todas as outras partes se conectam para trabalharem juntas. Escolher uma boa placa-mãe é importante porque ela determina quais componentes você pode usar e a capacidade de atualizar seu PC no futuro.';
-        carregarComponentes(componenteAtual);
-    }
-    else if (componenteAtual === 'Placa-mae') {
-        componenteAtual = 'RAM';
-        document.getElementById('resumoTit').innerText = 'Memória RAM';
-        document.getElementById('resumoDesc').innerText = 'É como a mesa de trabalho do computador, onde ele coloca as coisas que está usando no momento. Ter bastante RAM é importante para que seu computador possa fazer muitas coisas ao mesmo tempo sem ficar lento.';
-        carregarComponentes(componenteAtual);
-    }
-    else if (componenteAtual === 'RAM') {
-        componenteAtual = 'GPU';
-        document.getElementById('resumoTit').innerText = 'Placa de Vídeo (GPU)';
-        document.getElementById('resumoDesc').innerText = 'É como um artista que desenha as imagens e gráficos que você vê na tela. Escolher uma boa placa de vídeo é importante se você planeja jogar jogos, editar vídeos ou fazer qualquer coisa que envolva gráficos detalhados.';
-        carregarComponentes(componenteAtual);
-    }
-    else if (componenteAtual === 'GPU') {
-        componenteAtual = 'Armazenamento';
-        document.getElementById('resumoTit').innerText = 'Unidade de Armazenamento (HDD ou SSD)';
-        document.getElementById('resumoDesc').innerText = 'É como uma biblioteca onde o computador guarda todos os seus programas, documentos, fotos e outros arquivos. Escolher entre um HDD (mais barato e com mais espaço) e um SSD (mais rápido) é importante dependendo se você quer mais velocidade ou mais armazenamento.';
-        carregarComponentes(componenteAtual);
-    }
-    else if (componenteAtual === 'Armazenamento') {
-        componenteAtual = 'PSU';
-        document.getElementById('resumoTit').innerText = 'Fonte de Alimentação (PSU)';
-        document.getElementById('resumoDesc').innerText = 'É como a bateria do computador, que pega energia da tomada e distribui para todas as partes do computador. Escolher uma fonte de alimentação confiável e de boa qualidade é importante para garantir que todos os componentes recebam energia suficiente e para evitar problemas elétricos.';
-        carregarComponentes(componenteAtual);
+    // ATT VALOR PARCIAL NO LOCAL STORAGE
+    precoParcial += precoTemporario;
+    localStorage.setItem('totalParcial', precoParcial);
+    salvarComponentesSelecionados();
+    atualizarTotalParcial();
+    
+
+    // TROCANDO COMPONENTES VISIVEIS
+    switch (componenteAtual) {
+        case 'Processador':
+            componenteAtual = 'Placa-mae';
+            break;
+        case 'Placa-mae':
+            componenteAtual = 'RAM';
+            break;
+        case 'RAM':
+            componenteAtual = 'GPU';
+            break;
+        case 'GPU':
+            componenteAtual = 'Armazenamento';
+            break;
+        case 'Armazenamento':
+            componenteAtual = 'PSU';
+            break;
+        case 'PSU':
+            componenteAtual = 'Gabinete';
+            break;
+        case 'Gabinete':
+            componenteAtual = 'Refrigeração';
+            break;
+        case 'Refrigeração':
+            alert('salve');
+            return
     }
 
-    else if (componenteAtual === 'PSU') {
-        componenteAtual = 'Gabinete';
-        document.getElementById('resumoTit').innerText = 'Gabinete';
-        document.getElementById('resumoDesc').innerText = 'É a caixa que segura e protege todas as peças do computador, mantendo tudo organizado e seguro. Escolher um bom gabinete é importante para garantir que todas as peças cabem bem, que haja boa ventilação e que seja fácil de montar e manter.';
-        carregarComponentes(componenteAtual);
+    carregarComponentes(componenteAtual);
+
+    // RESUMO COMPONENTES
+    const resumos = {
+        'Placa-mae': ['Placa-Mãe', 'É como o centro de controle do computador, onde todas as outras partes se conectam para trabalharem juntas. Escolher uma boa placa-mãe é importante porque ela determina quais componentes você pode usar e a capacidade de atualizar seu PC no futuro.'],
+        'RAM': ['Memória RAM', 'É como a mesa de trabalho do computador, onde ele coloca as coisas que está usando no momento. Ter bastante RAM é importante para que seu computador possa fazer muitas coisas ao mesmo tempo sem ficar lento.'],
+        'GPU': ['Placa de Vídeo (GPU)', 'É como um artista que desenha as imagens e gráficos que você vê na tela. Escolher uma boa placa de vídeo é importante se você planeja jogar jogos, editar vídeos ou fazer qualquer coisa que envolva gráficos detalhados.'],
+        'Armazenamento': ['Armazenamento', 'É como uma biblioteca onde o computador guarda todos os seus programas, documentos, fotos e outros arquivos. Escolher entre um HDD (mais barato e com mais espaço) e um SSD (mais rápido) é importante dependendo se você quer mais velocidade ou mais armazenamento.'],
+        'PSU': ['Fonte de Alimentação (PSU)', 'É como a bateria do computador, que pega energia da tomada e distribui para todas as partes do computador. Escolher uma fonte de alimentação confiável e de boa qualidade é importante para garantir que todos os componentes recebam energia suficiente e para evitar problemas elétricos.'],
+        'Gabinete': ['Gabinete', 'É a caixa que segura e protege todas as peças do computador, mantendo tudo organizado e seguro. Escolher um bom gabinete é importante para garantir que todas as peças cabem bem, que haja boa ventilação e que seja fácil de montar e manter.'],
+        'Refrigeração': ['Cooler de CPU', 'É como um ventilador que mantém o cérebro do computador fresco para ele não esquentar demais. Escolher um bom cooler é importante para manter seu processador funcionando bem e evitar danos por superaquecimento.']
+    };
+    
+    // FORMATANDO O RESUMO PARA EXIBIR CORRETAMENTE
+    const [titulo, descricao] = resumos[componenteAtual] || [];
+    if (titulo && descricao) {
+        document.getElementById('resumoTit').innerText = titulo;
+        document.getElementById('resumoDesc').innerText = descricao;
     }
 
-    else if (componenteAtual === 'Gabinete') {
-        componenteAtual = 'Refrigeração';
-        document.getElementById('resumoTit').innerText = 'Cooler de CPU';
-        document.getElementById('resumoDesc').innerText = 'É como um ventilador que mantém o cérebro do computador fresco para ele não esquentar demais. Escolher um bom cooler é importante para manter seu processador funcionando bem e evitar danos por superaquecimento.';
-        carregarComponentes(componenteAtual);
-    }
-    else {
-        // Aqui você pode salvar os dados ou continuar para outros componentes
-        alert('Seleção final:', selectedComponent);
-    }
+
 })
 
 carregarDataComponentes()
-carregarValorTotal()
+atualizarTotalParcial()
