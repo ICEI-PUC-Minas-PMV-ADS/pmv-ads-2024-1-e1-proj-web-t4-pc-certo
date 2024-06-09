06f9ed65b5b2d4cf6467c5d1b8c3fc551cf30d79
Caeetanoo <caetano20062@gmail.com>
2024-06-09 20:31:37 -0300

    botoes resumo

--- codigo-fonte/js/ResumoDeComponentes.js
+++ codigo-fonte/js/ResumoDeComponentes.js
@@ -1,3 +1,40 @@
 const componentes = JSON.parse(localStorage.getItem('componentesSelecionados'))
 
 componentes.Processador.preco
+
+
+document.querySelectorAll('#alterarbotoes').forEach(function (element) {
+    element.addEventListener('click', function () {
+        window.location.href = '/codigo-fonte/EscolhaDeComponentes.html'
+
+    })
+});
+
+document.getElementById("comprarbotaoproc").addEventListener("click",function(){
+    window.location.href = (componentes.Processador.info)
+})
+
+document.getElementById("comprarbotaoplac").addEventListener("click",function(){
+    window.location.href = componentes.placa-mae.info
+})
+document.getElementById("comprarbotaomem").addEventListener("click",function(){
+    window.location.href = componentes.RAM.info
+})
+document.getElementById("comprarbotaovideo").addEventListener("click",function(){
+    window.location.href = componentes.GPU.info
+})
+document.getElementById("comprarbotaoarmaz").addEventListener("click",function(){
+    window.location.href = componentes.Armazenamento.info
+})
+document.getElementById("comprarbotaofont").addEventListener("click",function(){
+    window.location.href = componentes.PSU.info
+})
+document.getElementById("comprarbotaogabi").addEventListener("click",function(){
+    window.location.href = componentes.Gabinete.info
+})
+document.getElementById("comprarbotaocooler").addEventListener("click",function(){
+    window.location.href = componentes.Refrigeração.info
+})
+
+
+console.log(componentes.Processador.info)
