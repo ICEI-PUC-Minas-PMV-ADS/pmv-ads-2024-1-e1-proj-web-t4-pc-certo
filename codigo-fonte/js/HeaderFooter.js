4fffa825805ccb7365517612109c9dc497fe17b1
LcsToti <Lucasgabrielft01@gmail.com>
2024-06-09 19:20:50 -0300

    Alterações no HEADER

--- codigo-fonte/js/HeaderFooter.js
+++ codigo-fonte/js/HeaderFooter.js
@@ -1,16 +1,27 @@
-var sideBtn = document.querySelector('.sidebaricon');
+var sideBtn = document.querySelector(".sidebaricon");
 var togglenavbar = false;
-var navbar = document.querySelector('.botoescabecalho');
+var navbar = document.querySelector(".botoescabecalho");
 
+sideBtn.addEventListener("click", function () {
+  if (togglenavbar == false) {
+    navbar.style.right = "0";
+  } else {
+    navbar.style.right = "100vw";
+  }
+  togglenavbar = !togglenavbar;
+});
 
-sideBtn.addEventListener('click', function () {
-    if (togglenavbar == false) {
-        navbar.style.right = "0";
+//mudar icone login
 
-    }   
-    else {
-        navbar.style.right = "100vw";
+const temUsuario = localStorage.getItem("nomeCadastro");
 
-    }
-    togglenavbar = !togglenavbar;
-})
+document.getElementById(btLogado);
+document.getElementById(btDeslogado);
+
+btLogado.style.display = "none";
+btDeslogado.style.display = "flex";
+
+if (temUsuario) {
+  btLogado.style.display = "flex";
+  btDeslogado.style.display = "none";
+}
