880cfb5edbf5e0e2c4e11d15b6c9cd918336ce81
PedroBzra <pedro.fernandobzra@gmail.com>
2024-06-15 16:08:33 -0300

    Refactoring v2

--- codigo-fonte/js/EscolhadeComponentes.js
+++ codigo-fonte/js/EscolhadeComponentes.js
@@ -6,71 +6,6 @@ let precoParcial = parseFloat(localStorage.getItem('totalParcial')) || 0;
 let precoTemporario = 0;
 const ordemComponentes = ['Processador', 'PlacaMae', 'RAM', 'GPU', 'Armazenamento', 'PSU', 'Gabinete', 'Refrigeração'];
 
-// Atualizar resumo ao mudar de componente
-// 
-function atualizarResumo(componenteAtual) {
-    // Titulo de resumo e desc do resumo de componentes
-    const resumos = {
-        'Processador': ['Processador', 'É como o cérebro do computador, que pensa e realiza todas as tarefas. Escolher um processador rápido e eficiente é importante para garantir que seu computador possa realizar muitas tarefas de forma rápida e suave.', 'img/pagGuia-CPU.png'],
-        'PlacaMae': ['Placa-Mãe', 'É como o centro de controle do computador, onde todas as outras partes se conectam para trabalharem juntas. Escolher uma boa placa-mãe é importante porque ela determina quais componentes você pode usar e a capacidade de atualizar seu PC no futuro.', 'img/pagGuia-MB.jpg'],
-        'RAM': ['Memória RAM', 'É como a mesa de trabalho do computador, onde ele coloca as coisas que está usando no momento. Ter bastante RAM é importante para que seu computador possa fazer muitas coisas ao mesmo tempo sem ficar lento.', 'img/pagGuia-MemoriaRAM.png'],
-        'GPU': ['Placa de Vídeo (GPU)', 'É como um artista que desenha as imagens e gráficos que você vê na tela. Escolher uma boa placa de vídeo é importante se você planeja jogar jogos, editar vídeos ou fazer qualquer coisa que envolva gráficos detalhados.', 'img/pagGuia-GPU.png'],
-        'Armazenamento': ['Armazenamento', 'É como uma biblioteca onde o computador guarda todos os seus programas, documentos, fotos e outros arquivos. Escolher entre um HDD (mais barato e com mais espaço) e um SSD (mais rápido) é importante dependendo se você quer mais velocidade ou mais armazenamento.', 'img/pagGuia-Armazenamento.jpg'],
-        'PSU': ['Fonte de Alimentação (PSU)', 'É como a bateria do computador, que pega energia da tomada e distribui para todas as partes do computador. Escolher uma fonte de alimentação confiável e de boa qualidade é importante para garantir que todos os componentes recebam energia suficiente e para evitar problemas elétricos.', 'img/pagGuia-PSU.png'],
-        'Gabinete': ['Gabinete', 'É a caixa que segura e protege todas as peças do computador, mantendo tudo organizado e seguro. Escolher um bom gabinete é importante para garantir que todas as peças cabem bem, que haja boa ventilação e que seja fácil de montar e manter.', 'img/pagGuia-Gabinete.jpg'],
-        'Refrigeração': ['Cooler de CPU', 'É como um ventilador que mantém o cérebro do computador fresco para ele não esquentar demais. Escolher um bom cooler é importante para manter seu processador funcionando bem e evitar danos por superaquecimento.', 'img/pagGuia-Cooler.png']
-    };
-    // Mudando o resumo de acordo com o ComponenteAtual
-    const [titulo, descricao, imagem] = resumos[componenteAtual] || [];
-    if (titulo && descricao && imagem) {
-        document.getElementById('resumoTit').innerText = titulo;
-        document.getElementById('resumoDesc').innerText = descricao;
-        document.getElementById('resumoImg').src = imagem
-    }
-}
-// Calculo de atualização do Orçamento - Feedback ao exceder e outro ao passar 10% do valor total
-// 
-function atualizarOrcamentoCard() {
-    let orcamentoTotalString = localStorage.getItem('orcamentoTotal');
-    // Remove "R$" e "," para converter em número
-    let orcamentoTotal = parseFloat(orcamentoTotalString.replace('R$', '').replace('.', '').replace(',', '.')) || 0;
-    const margem = orcamentoTotal * 0.10;
-    const cardOrcamento = document.querySelector('#orcTotal');
-
-    if (precoParcial <= orcamentoTotal) {
-        cardOrcamento.parentElement.style.backgroundColor = '#308041';
-    } else if (precoParcial <= orcamentoTotal + margem) {
-        cardOrcamento.parentElement.style.backgroundColor = '#FFD600';
-        cardOrcamento.parentElement.style.color = 'black';
-    } else {
-        cardOrcamento.parentElement.style.backgroundColor = '#840000';
-        cardOrcamento.parentElement.style.color = 'white';
-        alert('O valor parcial excede o orçamento! Volte para selecionar suas peças novamente ou continue assim mesmo.');
-    }
-
-}
-// Funcao para atualizar Total Parcial 
-function atualizarTotalParcial() {
-    const TotalParcial = document.getElementById('parcialTotal');
-    TotalParcial.innerHTML = `R$${precoParcial.toFixed(2).replace('.', ',')}`;
-    atualizarOrcamentoCard()
-}
-
-// Salvar componentes selecionados no localStorage
-function salvarComponentesSelecionados() {
-    localStorage.setItem('componentesSelecionados', JSON.stringify(componenteSelecionado));
-}
-
-// Remover componente e preco dele ao voltar a peca anterior
-function removerComponenteSelecionado(tipo) {
-    if (componenteSelecionado[tipo]) {
-        precoParcial -= componenteSelecionado[tipo].preco;
-        delete componenteSelecionado[tipo];
-        localStorage.setItem('totalParcial', precoParcial);
-        salvarComponentesSelecionados();
-        atualizarTotalParcial();
-    }
-}
 
 // Funcao do btn Avancar
 document.getElementById('avancar').addEventListener('click', () => {
@@ -108,6 +43,7 @@ document.getElementById('voltar').addEventListener('click', () => {
     }
 
 });
+
 // Ao estar em processador remove localstorage para recomecar montagem
 if (componenteAtual === 'Processador') {
     localStorage.removeItem('componentesSelecionados');
@@ -118,14 +54,20 @@ if (componenteAtual === 'Processador') {
     atualizarTotalParcial();
 }
 
-// Carregar dados JSON
+
+carregarDataComponentes();
+atualizarTotalParcial();
+atualizarResumo(componenteAtual);
+
+
+// Fetch JSON
 async function carregarDataComponentes() {
     const resposta = await fetch('/codigo-fonte/js/JSON/Componentes.json');
     componentes = await resposta.json();
     carregarComponentes(componenteAtual);
 }
 
-// Transformar JSON em cards HTML
+// Transformar JSON em cards HTML ++ funcoes
 function carregarComponentes(tipo) {
 
     // log para teste -- descartavel
@@ -271,7 +213,68 @@ function carregarComponentes(tipo) {
     }
 }
 
+// Atualizar resumo ao mudar de componente
+function atualizarResumo(componenteAtual) {
+    // Titulo de resumo e desc do resumo de componentes
+    const resumos = {
+        'Processador': ['Processador', 'É como o cérebro do computador, que pensa e realiza todas as tarefas. Escolher um processador rápido e eficiente é importante para garantir que seu computador possa realizar muitas tarefas de forma rápida e suave.', 'img/pagGuia-CPU.png'],
+        'PlacaMae': ['Placa-Mãe', 'É como o centro de controle do computador, onde todas as outras partes se conectam para trabalharem juntas. Escolher uma boa placa-mãe é importante porque ela determina quais componentes você pode usar e a capacidade de atualizar seu PC no futuro.', 'img/pagGuia-MB.jpg'],
+        'RAM': ['Memória RAM', 'É como a mesa de trabalho do computador, onde ele coloca as coisas que está usando no momento. Ter bastante RAM é importante para que seu computador possa fazer muitas coisas ao mesmo tempo sem ficar lento.', 'img/pagGuia-MemoriaRAM.png'],
+        'GPU': ['Placa de Vídeo (GPU)', 'É como um artista que desenha as imagens e gráficos que você vê na tela. Escolher uma boa placa de vídeo é importante se você planeja jogar jogos, editar vídeos ou fazer qualquer coisa que envolva gráficos detalhados.', 'img/pagGuia-GPU.png'],
+        'Armazenamento': ['Armazenamento', 'É como uma biblioteca onde o computador guarda todos os seus programas, documentos, fotos e outros arquivos. Escolher entre um HDD (mais barato e com mais espaço) e um SSD (mais rápido) é importante dependendo se você quer mais velocidade ou mais armazenamento.', 'img/pagGuia-Armazenamento.jpg'],
+        'PSU': ['Fonte de Alimentação (PSU)', 'É como a bateria do computador, que pega energia da tomada e distribui para todas as partes do computador. Escolher uma fonte de alimentação confiável e de boa qualidade é importante para garantir que todos os componentes recebam energia suficiente e para evitar problemas elétricos.', 'img/pagGuia-PSU.png'],
+        'Gabinete': ['Gabinete', 'É a caixa que segura e protege todas as peças do computador, mantendo tudo organizado e seguro. Escolher um bom gabinete é importante para garantir que todas as peças cabem bem, que haja boa ventilação e que seja fácil de montar e manter.', 'img/pagGuia-Gabinete.jpg'],
+        'Refrigeração': ['Cooler de CPU', 'É como um ventilador que mantém o cérebro do computador fresco para ele não esquentar demais. Escolher um bom cooler é importante para manter seu processador funcionando bem e evitar danos por superaquecimento.', 'img/pagGuia-Cooler.png']
+    };
+    // Mudando o resumo de acordo com o ComponenteAtual
+    const [titulo, descricao, imagem] = resumos[componenteAtual] || [];
+    if (titulo && descricao && imagem) {
+        document.getElementById('resumoTit').innerText = titulo;
+        document.getElementById('resumoDesc').innerText = descricao;
+        document.getElementById('resumoImg').src = imagem
+    }
+}
 
-carregarDataComponentes();
-atualizarTotalParcial();
-atualizarResumo(componenteAtual);
+// Calculo de atualização do Orçamento - Feedback ao exceder e outro ao passar 10% do valor total
+function atualizarOrcamentoCard() {
+    let orcamentoTotalString = localStorage.getItem('orcamentoTotal');
+    // Remove "R$" e "," para converter em número
+    let orcamentoTotal = parseFloat(orcamentoTotalString.replace('R$', '').replace('.', '').replace(',', '.')) || 0;
+    const margem = orcamentoTotal * 0.10;
+    const cardOrcamento = document.querySelector('#orcTotal');
+
+    if (precoParcial <= orcamentoTotal) {
+        cardOrcamento.parentElement.style.backgroundColor = '#308041';
+    } else if (precoParcial <= orcamentoTotal + margem) {
+        cardOrcamento.parentElement.style.backgroundColor = '#FFD600';
+        cardOrcamento.parentElement.style.color = 'black';
+    } else {
+        cardOrcamento.parentElement.style.backgroundColor = '#840000';
+        cardOrcamento.parentElement.style.color = 'white';
+        alert('O valor parcial excede o orçamento! Volte para selecionar suas peças novamente ou continue assim mesmo.');
+    }
+
+}
+
+// Funcao para atualizar Total Parcial 
+function atualizarTotalParcial() {
+    const TotalParcial = document.getElementById('parcialTotal');
+    TotalParcial.innerHTML = `R$${precoParcial.toFixed(2).replace('.', ',')}`;
+    atualizarOrcamentoCard()
+}
+
+// Salvar componentes selecionados no localStorage
+function salvarComponentesSelecionados() {
+    localStorage.setItem('componentesSelecionados', JSON.stringify(componenteSelecionado));
+}
+
+// Remover componente e preco dele ao voltar a peca anterior
+function removerComponenteSelecionado(tipo) {
+    if (componenteSelecionado[tipo]) {
+        precoParcial -= componenteSelecionado[tipo].preco;
+        delete componenteSelecionado[tipo];
+        localStorage.setItem('totalParcial', precoParcial);
+        salvarComponentesSelecionados();
+        atualizarTotalParcial();
+    }
+}
